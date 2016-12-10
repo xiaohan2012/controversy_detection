c8a5ff86ae05dd05de2c81d5d7bddbe608cd5c66
Han Xiao <xiaohan2012@gmail.com>
2016-12-10 12:18:06 +0000

    fixed leopard: adding unknown nodes

--- leopard.py
+++ leopard.py
@@ -14,8 +14,14 @@ def get_alpha(g, k=2, gamma=GAMMA):
 def attempt_reassignment(g, n, partition, node2cluster, gamma, alpha):
     c_star = best_cluster_id(g, n, partition, gamma, alpha)
     did_it = False
-    if node2cluster[n] != c_star:
-        partition[node2cluster[n]].remove(n)
+    node_exist = (n in node2cluster)
+    if node_exist:
+        if node2cluster[n] != c_star:
+            partition[node2cluster[n]].remove(n)
+            partition[c_star].add(n)
+            node2cluster[n] = c_star
+            did_it = True
+    else:
         partition[c_star].add(n)
         node2cluster[n] = c_star
         did_it = True
@@ -36,7 +42,6 @@ def best_cluster_id(g, u, part, gamma, alpha):
     return max(part, key=score)
 
 
-
 def update_partition(g, node_cluster_ids, k=2,
                      edges_to_add=[], edges_to_remove=[],
                      gamma=GAMMA,
@@ -52,21 +57,24 @@ def update_partition(g, node_cluster_ids, k=2,
     if len(edges_to_add) == 0 and len(edges_to_remove) == 0:
         print('nothing to update')
         return node_cluster_ids
-    
+
+    assert len(node_cluster_ids) == g.number_of_nodes()
     partition = defaultdict(set)  # cluster id to list of nodes
     for p, n in zip(node_cluster_ids, g.nodes_iter()):
         partition[p].add(n)
     node2cluster = dict(zip(g.nodes(), node_cluster_ids))  # node to cluster id
     
     # print(node2cluster)
-    g.add_edges_from(edges_to_add)
-    g.remove_edges_from(edges_to_remove)
+    if edges_to_add:
+        g.add_edges_from(edges_to_add)
+    if edges_to_remove:
+        g.remove_edges_from(edges_to_remove)
 
     alpha = get_alpha(g, gamma=gamma)
     params = {'gamma': gamma, 'alpha': alpha}
     edge_iters = chain(edges_to_add, edges_to_remove)
     if verbose:
-        iters = tqdm(edge_iters)
+        edge_iters = tqdm(edge_iters)
 
     for u, v in edge_iters:
         if verbose:
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import random
+import string
 import networkx as nx
 
 
@@ -9,18 +10,24 @@ def build_graph_from_df(df):
     return g
 
 
-def sample_edges_to_add(g, n_edges_to_add):
+def sample_edges_to_add(g, n_edges_to_add,
+                        unknown_nodes_pool=list(string.ascii_lowercase)):
     n = 0
     edges_to_add = []
     nodes = g.nodes()
     while n < n_edges_to_add:
         while True:
             u = v = random.choice(nodes)
-            while u == v:
-                v = random.choice(nodes)
-            if not g.has_edge(u, v):
-                edges_to_add.append((u, v))
+            
+            if random.random() > 0.5:  # 0.5 proba to sample unknown nodes
+                edges_to_add.append((u, random.choice(unknown_nodes_pool)))
                 break
+            else:  # 0.5 proba to sample existing edge
+                while u == v:
+                    v = random.choice(nodes)
+                if not g.has_edge(u, v):
+                    edges_to_add.append((u, v))
+                    break
         n += 1
     return edges_to_add
 
@@ -38,8 +45,10 @@ class tw_list():
         i = 0
         while i < len(self.ts) and self.ts[i] < t:
             i += 1
+        removed_data = self.data[:i]
         self.ts = self.ts[i:]
         self.data = self.data[i:]
+        return removed_data
         
     def append(self, e, t):
         self.data.append(e)
