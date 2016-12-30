93cd0f75a1e70a0330d9fb62cce1924f07ac5a03
Han Xiao <xiaohan2012@gmail.com>
2016-12-30 20:47:26 +0000

    update graph bug fixed; top-k controversial hashtag integration

--- leopard.py
+++ leopard.py
@@ -2,6 +2,7 @@ import numpy as np
 from tqdm import tqdm
 from itertools import chain
 from collections import defaultdict
+from util import add_edges, remove_edges
 
 
 GAMMA = 1.5
@@ -12,6 +13,8 @@ def get_alpha(g, k=2, gamma=GAMMA):
 
 
 def attempt_reassignment(g, n, partition, node2cluster, gamma, alpha):
+    assert g.has_node(n)
+    
     c_star = best_cluster_id(g, n, partition, gamma, alpha)
     did_it = False
     node_exist = (n in node2cluster)
@@ -42,33 +45,38 @@ def best_cluster_id(g, u, part, gamma, alpha):
     return max(part, key=score)
 
 
-def update_partition(g, node_cluster_ids, k=2,
-                     edges_to_add=[], edges_to_remove=[],
+def update_partition(g,
+                     node2cluster,
+                     k=2,
+                     edges_to_add=[],
+                     edges_to_remove=[],
                      gamma=GAMMA,
                      verbose=True):
-    """
+    """                                                                                                                                                                             
     incrementally update the partitioning
+    node2cluster: node to cluster id
 
-    node_cluster_ids: list of cluster ids
-    
     return:
         list of updated cluster ids
     """
     if len(edges_to_add) == 0 and len(edges_to_remove) == 0:
         print('nothing to update')
-        return node_cluster_ids
+        return node2cluster
 
-    assert len(node_cluster_ids) == g.number_of_nodes()
-    partition = defaultdict(set)  # cluster id to list of nodes
-    for p, n in zip(node_cluster_ids, g.nodes_iter()):
-        partition[p].add(n)
-    node2cluster = dict(zip(g.nodes(), node_cluster_ids))  # node to cluster id
-    
+    assert len(node2cluster) == g.number_of_nodes(), '{} \n {}'.format(
+        set(node2cluster) - set(g.nodes()),
+        set(g.nodes()) - set(node2cluster)
+    )
+
     # print(node2cluster)
     if edges_to_add:
-        g.add_edges_from(edges_to_add)
+        add_edges(g, edges_to_add)
     if edges_to_remove:
-        g.remove_edges_from(edges_to_remove)
+        remove_edges(g, edges_to_remove)
+
+    partition = defaultdict(set)  # cluster id to list of nodes
+    for n, p in node2cluster.items():
+        partition[p].add(n)
 
     alpha = get_alpha(g, gamma=gamma)
     params = {'gamma': gamma, 'alpha': alpha}
@@ -79,6 +87,10 @@ def update_partition(g, node_cluster_ids, k=2,
     for u, v in edge_iters:
         if verbose:
             print('edge ({}, {})'.format(u, v))
+        if not g.has_node(u):
+            print(u)
+            print(edges_to_add)
+            print(edges_to_remove)
         affected_nodes = set()
         did_it, partition, node2cluster = attempt_reassignment(g, u,
                                                                partition,
@@ -88,6 +100,7 @@ def update_partition(g, node_cluster_ids, k=2,
             if verbose:
                 print('moved node {}'.format(u))
             affected_nodes |= set(g.neighbors(u))
+
         did_it, partition, node2cluster = attempt_reassignment(g, v,
                                                                partition,
                                                                node2cluster,
@@ -103,8 +116,16 @@ def update_partition(g, node_cluster_ids, k=2,
                     g, n, partition, node2cluster, **params)
                 if verbose:
                     if did_it:
-                        print('moved node {}'.format(n))      
+                        print('moved node {}'.format(n))
                     else:
                         print('{} stayed'.format(n))
 
-    return [node2cluster[n] for n in g.nodes_iter()]
+    # remove zero degree nodes
+    nodes_to_remove = [n for n in g.nodes_iter() if g.degree(n) == 0]
+    if nodes_to_remove:
+        # print('removing {} nodes'.format(len(nodes_to_remove)))
+        g.remove_nodes_from(nodes_to_remove)
+        for n in nodes_to_remove:
+            del node2cluster[n]
+                        
+    return node2cluster
